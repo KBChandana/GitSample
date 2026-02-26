6340f48142c48cc51f04dde69f68b741e3674a24
Baby Chandana Kamalapuram <bkamalapuram@innominds.com>
2026-02-26 12:59:10 +0530

    Initial Commit

---
+++ GitSample/GitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSampleApp.swift
+//  GitSample
+//
+//  Created by Baby Chandana Kamalapuram on 26/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
